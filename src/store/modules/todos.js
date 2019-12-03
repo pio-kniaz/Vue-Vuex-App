3bbe90f548d42b722de1d4dd456c24e7e3039b91
pio-kniaz <pio.kniaz@gmail.com>
2019-12-03 19:46:38 +0100

    WIP

---
+++ src/store/modules/todos.js
@@ -0,0 +1,49 @@
+import axios from "axios";
+
+// import axios from 'axios';
+
+const state = {
+  todos: [],
+};
+
+const getters = {
+  allTodos: (state) => state.todos,
+};
+
+const actions = {
+  async fetchTodos({ commit }) {
+    const { data } = await axios.get('https://jsonplaceholder.typicode.com/todos');
+    commit('setTodos', data);
+    console.log(data, 'data')
+  },
+  async addTodo({ commit }, title) {
+    const { data } = await axios.post('https://jsonplaceholder.typicode.com/todos', {
+      title,
+      completed: false,
+    });
+    commit('newTodo', data);
+  },
+  async deleteTodo( { commit }, id) {
+    await axios.delete(`https://jsonplaceholder.typicode.com/todos/${id}`);
+    commit('removeTodo', id);
+  }
+};
+
+const mutations = {
+  setTodos: (state, todos) => {
+    state.todos = todos
+  },
+  newTodo: (state, todo) => {
+    state.todos.unshift(todo)
+  },
+  removeTodo: (state, id) => {
+    (state.todos = state.todos.filter(todo => todo.id !== id))
+  },
+};
+
+export default {
+  state,
+  getters,
+  actions,
+  mutations,
+}
